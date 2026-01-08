506b2c4c476654de9142dffc3c9e697647692131
MadalinaChera <mail.madalina.chera@gmail.com>
2026-01-08 20:34:10 +0200

    Am terminat de refractorizat 2/3 teste cu pom

--- src/test/java/tests/CreateCustomerTest.java
+++ src/test/java/tests/CreateCustomerTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.*;
 
 import java.time.Duration;
 import java.util.List;
@@ -23,80 +24,32 @@ public class CreateCustomerTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        WebElement bankManagerElement = driver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
-        bankManagerElement.click();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginBankManager();
 
-        WebElement addCustomerElement = driver.findElement(By.xpath("//button[@ng-click='addCust()']"));
-        addCustomerElement.click();
+        ManagerPage managerPage = new ManagerPage(driver);
+        managerPage.createCustomer();
 
-        WebElement firstNameElement = driver.findElement(By.xpath("//input[@placeholder='First Name']"));
         String firstNameValue = "Madalina";
-        firstNameElement.sendKeys(firstNameValue);
-
-        WebElement lastNameElement = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
         String lastNameValue = "Chera";
-        lastNameElement.sendKeys(lastNameValue);
-
-        WebElement postCodeElement = driver.findElement(By.xpath("//input[@placeholder='Post Code']"));
         String postCodeValue = "307382";
-        postCodeElement.sendKeys(postCodeValue);
 
-        WebElement submitCustomerElement = driver.findElement(By.xpath("//button[@class='btn btn-default']"));
-        submitCustomerElement.click();
+        CustomerPage customerPage = new CustomerPage(driver);
+        customerPage.createCustomerProcess(firstNameValue, lastNameValue, postCodeValue);
+        customerPage.openAccount();
 
-        Alert customerAlert = driver.switchTo().alert();
-        String customerAlertText = customerAlert.getText();
-        System.out.println(customerAlertText);
-        customerAlert.accept();
-
-        WebElement openAccountElement = driver.findElement(By.xpath("//button[@ng-click='openAccount()']"));
-        openAccountElement.click();
-
-        WebElement customerNameElement = driver.findElement(By.id("userSelect"));
-        Select customerSelect = new Select(customerNameElement);
         String fullName = firstNameValue + " " + lastNameValue;
-        customerSelect.selectByVisibleText(fullName);
-
-        WebElement currencyElement = driver.findElement(By.id("currency"));
-        Select currencySelect = new Select(currencyElement);
         String currencyValue = "Dollar";
-        currencySelect.selectByVisibleText(currencyValue);
 
-        WebElement processButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        processButton.click();
+        AccountPage accountPage = new AccountPage(driver);
+        accountPage.createAccountProcess(fullName, currencyValue);
 
-        Alert accountAlert = driver.switchTo().alert();
-        String accountAlertText = accountAlert.getText();
-        System.out.println(accountAlertText);
-        String [] accountArray = accountAlertText.split(":");
-        String accountNumber = accountArray[1];
-        System.out.println(accountArray[1]);
-        accountAlert.accept();
+        accountPage.openCustomerspage();
 
-        WebElement customerElement = driver.findElement(By.xpath("//button[@ng-click='showCust()']"));
-        customerElement.click();
-
-        // Adaugam un wait fortat care sa ne garanteze ca cele 2 componente comunica intre ele
-
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//        }
-
-        WebElement searchCustomerElement = driver.findElement(By.xpath("//input[@placeholder='Search Customer']"));
-        searchCustomerElement.click();
-        searchCustomerElement.sendKeys(firstNameValue);
-
-        List<WebElement> tableRows = driver.findElements(By.xpath("//tbody/tr"));
-        String customerTableRow = tableRows.get(0).getText();
-        Assert.assertTrue(customerTableRow.contains(firstNameValue));
-        Assert.assertTrue(customerTableRow.contains(lastNameValue));
-        Assert.assertTrue(customerTableRow.contains(postCodeValue));
-        Assert.assertTrue(customerTableRow.contains(accountNumber));
-
-        WebElement deleteCustomerElement = driver.findElement(By.xpath("//button[@ng-click='deleteCust(cust)']"));
-        deleteCustomerElement.click();
-
+        CustomersPage customersPage = new CustomersPage(driver);
+        customersPage.searchCustomer(firstNameValue);
+        customersPage.validateCustomer(firstNameValue, lastNameValue, postCodeValue);
+        customersPage.deleteCustomer();
 
     }
 }
--- src/test/java/tests/CreateCustomersTest.java
+++ src/test/java/tests/CreateCustomersTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+import pages.CustomerPage;
+import pages.LoginPage;
+import pages.ManagerPage;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -23,37 +26,18 @@ public class CreateCustomersTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        WebElement bankManagerElement = driver.findElement(By.xpath("//button[text()='Bank Manager Login']"));
-        bankManagerElement.click();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginBankManager();
 
-        WebElement addCustomerElement = driver.findElement(By.xpath("//button[@ng-click='addCust()']"));
-        addCustomerElement.click();
+        ManagerPage managerPage = new ManagerPage(driver);
+        managerPage.createCustomer();
 
         List<String> firstNameValueList = Arrays.asList("Mada1", "Mada2", "Mada3");
         List<String> lastNameValueList = Arrays.asList("Chera1", "Chera2", "Chera3");
         List<String> postCodeValueList = Arrays.asList("123", "456", "789");
-        String fullName = "";
-        int i = 0;
-        while (i<firstNameValueList.size()) {
-            WebElement firstNameElement = driver.findElement(By.xpath("//input[@placeholder='First Name']"));
-            firstNameElement.sendKeys(firstNameValueList.get(i));
 
-            WebElement lastNameElement = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
+        CustomerPage customerPage = new CustomerPage(driver);
+        customerPage.createCustomersProcess(firstNameValueList, lastNameValueList, postCodeValueList);
 
-            lastNameElement.sendKeys(lastNameValueList.get(i));
-
-            WebElement postCodeElement = driver.findElement(By.xpath("//input[@placeholder='Post Code']"));
-            postCodeElement.sendKeys(postCodeValueList.get(i));
-
-            WebElement submitCustomerElement = driver.findElement(By.xpath("//button[@class='btn btn-default']"));
-            submitCustomerElement.click();
-
-            Alert customerAlert = driver.switchTo().alert();
-            String customerAlertText = customerAlert.getText();
-            System.out.println(customerAlertText);
-            customerAlert.accept();
-            fullName = firstNameValueList.get(i) + " " + lastNameValueList.get(i);
-            i++;
-        }
     }
 }
